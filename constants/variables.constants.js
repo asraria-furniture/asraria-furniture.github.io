f7617d8b9fc558a25f5a54111b99ae8186942fc4
tanweerhossain <tanweerhossainatspacebbsr@gmail.com>
2023-11-26 16:33:27 +0530

    Build header and sales blank page with few SEO

--- constants/variables.constants.js
+++ constants/variables.constants.js
@@ -1,7 +1,13 @@
 export const VAR = {
     // Technology Specific Nomenlatures
     TITLE: "title",
-    SPAN: "span"
+    SPAN: "span",
+    IMG: "img",
+    // Trigger on input focus out
+    EVENT_CHANGE: "change",
+    // Trigger on input change
+    EVENT_INPUT: "input",
 
     // Product Specific Nomenlatures
+    MEDIA_BEARKPOINT_WIDTH: 1200,
 };
--- index.js
+++ index.js
@@ -4,13 +4,20 @@ import { VAR } from "./constants/variables.constants.js";
 import { LOCAL_VAR } from "./local-variables.constants.js";
 
 export class Home {
+    constructor() {
+        this.searchText = "";
+    }
     static onInit() {
         const orgInfo = new OrgInfo();
         const home = new Home();
-        
+
         orgInfo.setTitle();
         home.setLogo();
         home.setDepartments();
+        home.listenSearchInput();
+        home.listenSearchInputCloseClick();
+
+        return home;
     }
     setLogo() {
         const tagRef = document.getElementById(LOCAL_VAR.MAIN_LOGO_ID);
@@ -26,11 +33,36 @@ export class Home {
             if (Object.hasOwnProperty.call(CONF.DEPARTMENTS, departmentKey)) {
                 const departmentValue = CONF.DEPARTMENTS[departmentKey];
                 const spanRef = document.createElement(VAR.SPAN);
-        
+
                 spanRef.textContent = departmentValue;
                 tagRef.appendChild(spanRef);
             }
         }
+    }
+    onSearch(searchText) {
+        const iconRef = document.getElementById(LOCAL_VAR.MAIN_HEADER_SEARCH_INPUT_CLOSE_ICON_ID);
+        
+        this.searchText = searchText;
+        iconRef.style.display = searchText ? "block" : "none";
+    }
+    onClickClearSearchInput() {
+        const inputRef = document.getElementById(LOCAL_VAR.MAIN_HEADER_SEARCH_INPUT_ID);
+        
+        inputRef.value = "";
+        this.onSearch("");
+        inputRef.focus();
+    }
+    listenSearchInput() {
+        const inputRef = document.getElementById(LOCAL_VAR.MAIN_HEADER_SEARCH_INPUT_ID);
+        
+        if (window.innerWidth < VAR.MEDIA_BEARKPOINT_WIDTH) return;
+        
+        inputRef.oninput = (event) => event.target ? this.onSearch(event.target.value) : null;
+    }
+    listenSearchInputCloseClick() {
+        const closeIconRef = document.getElementById(LOCAL_VAR.MAIN_HEADER_SEARCH_INPUT_CLOSE_ICON_ID);
+        
+        closeIconRef.onclick = () => this.onClickClearSearchInput();
     }
 }
 
